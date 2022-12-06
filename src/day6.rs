1205feb60b40e35d0e96f8632611e460d687d249
Aoife Bradley <me@ifa.codes>
2022-12-06 03:48:24 -0500

    Update day 6

--- src/day6.rs
+++ src/day6.rs
@@ -3,19 +3,24 @@ use std::io::{BufRead, BufReader, Error, Read};
 
 pub fn day_6(io: BufReader<impl Read>) -> Result<()> {
     let buffer = io.lines().take(1).collect::<Result<String, Error>>()?;
-    let part_1 = buffer
-        .as_bytes()
-        .windows(4)
-        .position(|x| !(1..x.len()).any(|i| x[i..].contains(&x[i - 1])))
-        .ok_or_else(|| anyhow!("Couldn't find the index!"))?
-        + 4;
-
-    let part_2 = buffer
-        .as_bytes()
-        .windows(14)
-        .position(|x| !(1..x.len()).any(|i| x[i..].contains(&x[i - 1])))
-        .ok_or_else(|| anyhow!("Couldn't find the index!"))?
-        + 14;
+    let (part_1, part_2) = [4, 14]
+        .iter()
+        .map(|n| {
+            buffer
+                .as_bytes()
+                .windows(*n)
+                .position(|x| !(1..x.len()).any(|i| x[i..].contains(&x[i - 1])))
+                .expect("Couldn't find the index!")
+                + n
+        })
+        .fold((0, 0), |mut acc, a| {
+            if acc.0 == 0 {
+                acc.0 = a
+            } else {
+                acc.1 = a
+            }
+            acc
+        });
 
     println!("Day 6 - Part 1: {part_1}\nDay 6 - Part 2: {part_2}");
     Ok(())
